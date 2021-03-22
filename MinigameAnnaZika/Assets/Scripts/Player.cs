85cf1681a5ce5bfb8d2fc48c64c783ca73051b98
AnnaZika <annacarmen0@gmail.com>
2021-03-22 13:14:53 +0100

    Add Unity Project

---
+++ MinigameAnnaZika/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    public float speed = 3f;
+    public Rigidbody rb;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        rb.useGravity = true;
+
+        transform.position = pos;
+    }
+}
